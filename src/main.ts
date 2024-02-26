9c98d6874e5c2e3069d2c7081ab90958657c0d2b
Urias-Flores <93843706+Urias-Flores@users.noreply.github.com>
2024-02-25 21:31:38 -0600

    ssl

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import fs from 'fs';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+
+  const httpsOptions = {
+    key: fs.readFileSync('/home/buffete/certificates/grupo-sosamorales/private.key'),
+    cert: fs.readFileSync('/home/buffete/certificates/grupo-sosamorales/certificate.crt'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
+
   app.setGlobalPrefix('/api');
   app.enableCors();
+
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
