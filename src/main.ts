68b8cbf2f398fa34e98ffb77bc1e67c4d6e16dc8
Urias-Flores <93843706+Urias-Flores@users.noreply.github.com>
2024-02-28 13:11:47 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,13 @@ import * as fs from 'fs';
 async function bootstrap() {
   dotenv.config();
 
-  /*
   const httpsOptions = {
     key: fs.readFileSync('/home/buffete/certificates/grupo-sosamorales/private.key'),
     cert: fs.readFileSync('/home/buffete/certificates/grupo-sosamorales/certificate.crt'),
     ca: fs.readFileSync('/home/buffete/certificates/grupo-sosamorales/ca_bundle.crt'),
   }
-  */
 
-  const app = await NestFactory.create(AppModule /*, { httpsOptions }*/);
+  const app = await NestFactory.create(AppModule , { httpsOptions });
   app.setGlobalPrefix('/api');
   app.enableCors();
   await app.listen(process.env.PORT || 8000);
